1cf57406fb5447d14cb7990d1d3e3f3a318c22a3
Erik Pereira <erik.p.f.2001@gmail.com>
2025-10-30 08:52:21 +0100

    First commit for the AD5611 Arduino library.

---
+++ src/AD5611.cpp
@@ -0,0 +1,57 @@
+#include "AD5611.h"
+
+#define DAC_SPI_FREQUENCY 10000000 
+/**
+ * @brief AD5611 DAC driver class.
+ *
+ * This class provides an interface for controlling the AD5611 Digital-to-Analog Converter
+ * via SPI. It supports writing 10-bit values to the DAC.
+ */
+AD5611::AD5611(int syncPin) {
+    _csPin = syncPin;
+}
+
+void AD5611::begin() {
+    pinMode(_csPin, OUTPUT);
+    digitalWrite(_csPin, HIGH); 
+
+    SPI.begin(); 
+}
+
+/**
+ * @brief Writes a digital value to the AD5611 Digital-to-Analog Converter (DAC).
+ *
+ * This function takes a 12-bit digital value and sends it to the AD5611 DAC
+ * via SPI. It first checks if the provided value is within the DAC's
+ * configured resolution. If valid, it constructs the command byte by
+ * combining the DAC's normal operating mode with the input value,
+ * then transfers the high and low bytes over the SPI bus.
+ *
+ * @param value The 10-bit digital value to be written to the DAC.
+ *              This value should be less than (1 << _resolution).
+ * @return int Returns 0 on successful write, or -1 if the provided value
+ *             exceeds the DAC's configured resolution.
+ */
+int AD5611::writeValue(uint16_t value) {
+    if (value >= (1 << _resolution)) {
+        return -1; 
+    }
+
+    uint16_t command = DAC_AD56X1_MODE_NORMAL; 
+    
+    command |= value << 4; 
+    uint8_t highByte = (uint8_t)(command >> 8);
+    uint8_t lowByte = (uint8_t)(command & 0xFF);
+
+    SPI.beginTransaction(SPISettings(DAC_SPI_FREQUENCY, MSBFIRST, SPI_MODE1));
+
+    digitalWrite(_csPin, LOW);
+
+    SPI.transfer(highByte);
+    SPI.transfer(lowByte);
+
+    digitalWrite(_csPin, HIGH);
+    SPI.endTransaction();
+
+    return 0;
+}
---
+++ src/AD5611.h
@@ -0,0 +1,41 @@
+#ifndef AD5611_H
+#define AD5611_H
+
+#include <Arduino.h>
+#include <SPI.h> 
+
+/**
+ * @brief Class for controlling the AD5611 Digital-to-Analog Converter
+ * 
+ * The AD5611 is a 10-bit Digital-to-Analog Converter (DAC) with SPI interface.
+ * This class provides methods to initialize and control the DAC.
+ * 
+ * @note Operating voltage range: 2.7V to 5.5V
+ * @note Resolution: 10-bit
+ * 
+ * Power modes available:
+ * - Normal operation
+ * - Power-down with 1kΩ to GND
+ * - Power-down with 100kΩ to GND
+ * - Power-down in three-state
+ */
+class AD5611 {
+public:
+    
+    AD5611(int syncPin);
+    
+    void begin();
+
+    int writeValue(uint16_t value);
+
+private:
+    int _csPin;
+
+    const uint8_t _resolution = 10;
+    const uint16_t DAC_AD56X1_MODE_NORMAL = 0x0000; 
+    const uint16_t DAC_AD56X1_MODE_POWER_DOWN_1K = 0x4000;
+    const uint16_t DAC_AD56X1_MODE_POWER_DOWN_100K = 0x8000;
+    const uint16_t DAC_AD56X1_MODE_POWER_DOWN_THREE_STATE = 0xC000;
+};
+
+#endif
